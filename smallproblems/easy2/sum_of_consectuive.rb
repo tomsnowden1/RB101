72355e97c8badfaf02ca1a15a976e647c7493619
Tom Snowden <tom.snowden1@gmail.com>
2020-11-16 09:14:03 +0000

    updating consecutive

--- smallproblems/easy2/sum_of_consectuive.rb
+++ smallproblems/easy2/sum_of_consectuive.rb
@@ -1,5 +1,25 @@
-def sum_or_product
-  puts ""
+
+
+loop do 
+    puts "Please enter a number greater than 0"
+    number = gets.chomp.to_i
+    number_array = [*1..number]
+    
+    puts ">> Enter 's' to compute the sum, 'p' to compute the product."
+    choice = gets.chomp
+    
+    if choice == "s"
+       answer_add = number_array.each.inject(:+)
+       puts "The sum of the integers between 1 and #{number} is #{answer_add}"
+       else
+           choice == "p"
+       answer_multiply = number_array.each.inject(:*)
+       puts "The product of the integers between 1 and #{number} is #{answer_multiply}"
+     end
+     
+     puts "do you want to play again? [y or n]"
+     play_again = gets.chomp.downcase
+     break unless play_again == "y"
 end
 
 
